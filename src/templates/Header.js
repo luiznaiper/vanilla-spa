b91c5e9336e307171f7b9ec09e0f728211a996a8
luiznaiper <luiz_naiper@hotmail.com>
2022-12-21 08:15:45 -0300

    :construction: Class 10

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
             </h1>
         </div>
         <div class="Header-nav">
-        <a href="#/abour/">
+        <a href="#/about/">
         About
         </a>
         </div>
